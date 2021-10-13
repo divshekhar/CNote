126d3332a029c63cc017c4e8007a78138ed96350
Arpit Sahu <56678108+Arpit-Sahu@users.noreply.github.com>
2021-10-13 09:09:43 +0530

    added comments

--- main.c
+++ main.c
@@ -24,6 +24,7 @@ int main(int argc,char **argv)
 {
     GtkApplication *app;
     int status;
+    //initialize app
     app = gtk_application_new ("com.hackthedeveloper", G_APPLICATION_FLAGS_NONE);
     g_signal_connect (app, "activate", G_CALLBACK(activate), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
