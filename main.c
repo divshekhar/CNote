c251ae32dc3a2a38e5426e33e9c8d0fad2b2cecd
Shubhra1906 <92198373+Shubhra1906@users.noreply.github.com>
2021-10-16 13:16:29 +0530

    Update main.c

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ void changeStatus(GtkWidget *statusBar,guint statusContext,gchar *str)
 static void activate (GtkApplication* app, gpointer user_data)
 {
     GtkWidget *window;
+    // initialize window
     window = gtk_application_window_new (app);
     gtk_window_set_title (GTK_WINDOW (window), "CNote");
     gtk_window_set_default_size (GTK_WINDOW (window), 800, 500);
